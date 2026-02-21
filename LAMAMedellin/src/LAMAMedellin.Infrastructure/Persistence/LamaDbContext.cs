cf893f5e432dc2be0a24bc90e187edbb4d96268f
Daniel Villamizar <daniel8106@hotmail.com>
2026-02-21 02:04:56 -0500

    feat(infra): agregar seeder avanzado de miembros

--- LAMAMedellin/src/LAMAMedellin.Infrastructure/Persistence/LamaDbContext.cs
+++ LAMAMedellin/src/LAMAMedellin.Infrastructure/Persistence/LamaDbContext.cs
@@ -8,6 +8,7 @@ public sealed class LamaDbContext(DbContextOptions<LamaDbContext> options) : DbC
 {
     public DbSet<Banco> Bancos => Set<Banco>();
     public DbSet<CentroCosto> CentrosCosto => Set<CentroCosto>();
+    public DbSet<Miembro> Miembros => Set<Miembro>();
     public DbSet<Transaccion> Transacciones => Set<Transaccion>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ LAMAMedellin/src/LAMAMedellin.Infrastructure/Persistence/Seeders/LamaDbContextSeed.cs
@@ -0,0 +1,11 @@
+using LAMAMedellin.Infrastructure.Persistence.Seeders;
+
+namespace LAMAMedellin.Infrastructure.Persistence;
+
+public static class LamaDbContextSeed
+{
+    public static async Task SeedAsync(this LamaDbContext context)
+    {
+        await context.SeedMiembrosAsync();
+    }
+}
